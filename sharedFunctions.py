c20847dd7454cd585d07fbf1300145dfd94048ed
LucasWBritz <lucas.wi.britz@gmail.com>
2020-03-31 17:26:14 -0300

    Scrape posted on date

--- sharedFunctions.py
+++ sharedFunctions.py
@@ -1,11 +1,18 @@
 from bs4 import BeautifulSoup
-import urllib.request
+from urllib.request import urlopen, Request
+import urllib.parse
 
 def downloadLink(link):
-    return urllib.request.urlopen(link).read().decode("utf-8")
+    req = urllib.request.Request(link, 
+        headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+    )
+    return urllib.request.urlopen(req).read().decode("utf-8")
 
 def getAttrFromTag(tag, name):
     return tag.get(name)
 
 def downloadAndParseLink(link):
     return BeautifulSoup(downloadLink(link), 'html.parser')
+
+def encodeString(string):
+    return urllib.parse.quote(string)
